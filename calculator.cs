25850378fd5bd9fc5c739afb8909792e497318e3
onny <onny@students.amikom.ac.id>
2020-03-12 19:46:36 +0700

    Menambahkan Input Nilai

--- calculator.cs
+++ calculator.cs
@@ -1,7 +1,31 @@
-class Calculator
+using System;
+
+namespace Calculator
 {
-		static void Main(String[] args)
-		{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            Console.Write("Pilih Menu Calculator");
+            Console.WriteLine();
+			Console.WriteLine();
+            Console.WriteLine("1. Penambahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
 			
-		}
+			Console.WriteLine();
+			
+            Console.Write("Inputkan Nomer Menu 1 - 4 : ");
+            int c = int.Parse(Console.ReadLine());
+			
+            Console.Write("Inputkan Nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputkan Nilai b = ");
+            int b = int.Parse(Console.ReadLine());
+        }
+    }
 }
