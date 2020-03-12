41f3978b40829134922a4db01a328f32be772059
onny <onny@students.amikom.ac.id>
2020-03-12 19:49:18 +0700

    Menambahkan Fungsi Aritmatika

--- calculator.cs
+++ calculator.cs
@@ -27,5 +27,25 @@ namespace Calculator
             Console.Write("Inputkan Nilai b = ");
             int b = int.Parse(Console.ReadLine());
         }
+		
+		static int Penambahan(int a, int b)
+		{
+				return a + b;
+		}
+		
+		static int Pengurangan(int a, int b)
+		{
+				return a - b;
+		}
+		
+		static int Pembagian(int a, int b)
+		{
+				return a / b;
+		}
+		
+		static int Perkalian(int a, int b)
+		{
+				return a * b;
+		}
     }
 }
